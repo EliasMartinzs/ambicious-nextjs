27fdeabae3f0d197400afb0bcff0c9c98623f51a
elias <rhyanlucaseliasmartins@gmail.com>
2023-11-17 12:59:02 -0300

    done ?

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
   return (
-    <main className="w-full h-screen flex-center flex-col bg-[#19191a] text-white">
+    <main className="w-full h-screen flex-center flex-col text-black">
       <div className="flex-center flex-col py-10">
         <h3 className="title font-black">Ambicious</h3>
         <p>Faca login para ter acesso a plataforma</p>
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,13 @@
 import { SignUp } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <main className="w-full h-screen flex-center flex-col text-black">
+      <div className="flex-center flex-col py-10">
+        <h3 className="title font-black">Ambicious</h3>
+        <p>Criar uma conta para ter acesso a plataforma</p>
+      </div>
+      <SignUp />
+    </main>
+  );
 }
