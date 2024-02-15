1a7f1d42bc3a4284865b6921cb8121064a35fe8f
Elias Maritins <97196276+EliasMartinzs@users.noreply.github.com>
2024-02-15 11:47:09 -0600

    Update user.ts

--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -2,8 +2,12 @@ import * as z from 'zod';
 
 export const UserValidation = z.object({
   profile_photo: z.string().url().nonempty(),
-  name: z.string().min(3).max(30),
-  username: z.string().min(3).max(30),
+  name: z.string().min(1, {
+    message: 'Por favor insira um nome para continuar!'
+  }).max(30),
+  username: z.string().min(1,{
+  message: 'Por favor insira um sobrenome para continuar!'
+  }).max(30),
   bio: z.string().min(3).max(1000),
 });
 
